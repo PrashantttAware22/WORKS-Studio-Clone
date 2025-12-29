0ab31dcb8c158958805afaf97af9860d8c25e3de
PrashantttAware22 <prashantaware22@gmail.com>
2025-12-29 12:50:26 +0530

    modified JS

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let elems = document.querySelectorAll(".elem");
 let h2 = document.querySelectorAll(".elem h2");
 let page2 = document.querySelector(".page2");
 
-
 Shery.mouseFollower() ;
 Shery.makeMagnet(".magnet") ;
 document.querySelector("footer #back").addEventListener("click" , function() {
